e8a62f663b5638434810159f8a0f5bcd4d80e489
Peter Steenbergen <psteenbergen@gmail.com>
2019-08-21 09:13:53 +0000

    Apply fixes from StyleCI

--- tests/MessagebirdClientTest.php
+++ tests/MessagebirdClientTest.php
@@ -2,11 +2,11 @@
 
 namespace NotificationChannels\Messagebird\Test;
 
-use GuzzleHttp\Client;
 use Mockery;
+use GuzzleHttp\Client;
+use PHPUnit_Framework_TestCase;
 use NotificationChannels\Messagebird\MessagebirdClient;
 use NotificationChannels\Messagebird\MessagebirdMessage;
-use PHPUnit_Framework_TestCase;
 
 class MessagebirdClientTest extends PHPUnit_Framework_TestCase
 {
