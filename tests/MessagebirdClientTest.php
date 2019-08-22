fb89261d625117b50ec959ffeff97ad32e18cfe0
Tonko Mulder <t.mulder@jvdmedia.nl>
2019-08-22 20:59:14 +0200

    update setUp and tearDown to use void as return type
    for compatibility with phpunit version
    
    - setting php requirement to ^7.1
    - dropping 7.0 support due to EOL
    - fixed platform setting for php
    - bumping minimal version of php to 7.2 due phpunit requirements
    - php 7.1 is EOL pretty soon
    - added assertNull on tests which were missing asserts
    - dropped 7.1 on Travis builds
    - phpunit requires 7.2 and up
    
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- tests/MessagebirdClientTest.php
+++ tests/MessagebirdClientTest.php
@@ -4,20 +4,20 @@ namespace NotificationChannels\Messagebird\Test;
 
 use Mockery;
 use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
 use NotificationChannels\Messagebird\MessagebirdClient;
 use NotificationChannels\Messagebird\MessagebirdMessage;
-use PHPUnit\Framework\TestCase;
 
 class MessagebirdClientTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         $this->guzzle = Mockery::mock(new Client());
         $this->client = Mockery::mock(new MessagebirdClient($this->guzzle, 'test_ek1qBbKbHoA20gZHM40RBjxzX'));
         $this->message = (new MessagebirdMessage('Message content'))->setOriginator('APPNAME')->setRecipients('31650520659')->setReference('000123');
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         Mockery::close();
         parent::tearDown();
@@ -33,6 +33,7 @@ class MessagebirdClientTest extends TestCase
     /** @test */
     public function it_can_send_message()
     {
-        $this->client->send($this->message);
+        $this->client->shouldReceive('send')->with($this->message)->once();
+        $this->assertNull($this->client->send($this->message));
     }
 }
